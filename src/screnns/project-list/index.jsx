f4034c97c5e0166aff890d1c87e551faed88abc8
244686710 <244686710@qq.com>
2021-08-01 19:44:41 +0800

    feat: 添加 useMount 和 useDebounce 连个hook

---
+++ src/screnns/project-list/index.jsx
@@ -0,0 +1,37 @@
+
+import React, {useEffect, useState} from 'react'
+import { List } from './list'
+import { SearchPanel } from './search-panel'
+import qs from 'qs'
+import {clearnObject, useMount, useDebounce} from 'utils'
+
+const apiUrl = process.env.REACT_APP_API_URL
+export const ProjectListScreen = () => {
+  const [param, setParam] = useState({
+    name: '',
+    personId: ''
+  })
+  const debounceParam = useDebounce(param, 2000)
+  const [list, setList] = useState([])
+  const [users, setUsers] = useState([])
+  useEffect(() => {
+    fetch(`${apiUrl}/projects?${qs.stringify(clearnObject(debounceParam))}`).then(async response => {
+      if (response.ok) {
+        setList(await response.json())
+      }
+    })
+  }, [debounceParam])
+  useMount(() => {
+    fetch(apiUrl + '/users').then(async response => {
+      if (response.ok) {
+        setUsers(await response.json())
+      }
+    })
+  })
+  return (
+    <div>
+      <SearchPanel param={param} setParam={setParam} users={users}/>
+      <List list={list} users={users} />
+    </div>
+  )
+}
